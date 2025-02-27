7f3195e743b2a760251c259928e02062afac67c5
Awowz <tonygriffith75@gmail.com>
2025-02-26 21:13:12 -0600

    added leafnode class and test case

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,6 @@
 
 
-class HTMLNode():
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -8,7 +8,7 @@ class HTMLNode():
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError()
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
         if self.props == None or not isinstance(self.props, dict): return ""
@@ -19,3 +19,14 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode -> tag: {self.tag} | value: {self.value} | props: {self.props_to_html()}\nchildren: {self.children}"
+    
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None: raise ValueError("LeafNode self.value must have a value")
+        if self.tag == None: return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
