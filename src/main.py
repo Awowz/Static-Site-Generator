b0f61a05469428cd213adc4f8806fa630a85b858
Awowz <tonygriffith75@gmail.com>
2025-03-21 22:35:00 -0500

    preparing code for github pages

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os
 import shutil
+import sys
 from textnode import *
 from blocknode import markdown_to_html_node
 
-PUBLIC_PATH = "public"
-STATIC_PATH = "static"
+PUBLIC_PATH = "./doc"
+STATIC_PATH = "./static"
 CONTENT_PATH = "./content"
 TEMPLET_FILE = "template.html"
-PUBLIC_PATH = "./public"
 
 
 def extract_title(markdown):
@@ -16,7 +16,7 @@ def extract_title(markdown):
         raise Exception("file contiants no starter header")
     return lines[0][2:]
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     f = open(from_path, 'r')
     file_contents = f.read()
@@ -27,19 +27,21 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(file_contents)
     template_contents = template_contents.replace("{{ Title }}", title)
     template_contents = template_contents.replace("{{ Content }}", html)
+    template_contents = template_contents.replace("href=\"/", f"href=\"{base_path}")
+    template_contents = template_contents.replace("src=\"/", f"src=\"{base_path}")
     dest_path = dest_path.replace(".md", ".html")
     dest = open(dest_path, "w+")
     dest.write(template_contents)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):#note for me. code is done, but html server isnt diplaying html nor index??
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for file_object in os.listdir(dir_path_content):
         full_object_path = os.path.join(dir_path_content, file_object)
         if os.path.isfile(full_object_path):
-            generate_page(full_object_path, template_path, os.path.join(dest_dir_path, file_object))
+            generate_page(full_object_path, template_path, os.path.join(dest_dir_path, file_object), basepath)
         else:
             full_destination_path = os.path.join(dest_dir_path, file_object)
             os.mkdir(full_destination_path)
-            generate_pages_recursive(full_object_path,template_path, full_destination_path)
+            generate_pages_recursive(full_object_path,template_path, full_destination_path, basepath)
 
 def clear_public():
     if not os.path.exists(PUBLIC_PATH):
@@ -66,8 +68,13 @@ def generate_new_public_dir():
     recursive_file_copy(STATIC_PATH, PUBLIC_PATH)
 
 def main():
+    basepath = "/"
+    if len(sys.argv) >= 2:
+        basepath = sys.argv[1]
+    print(f"basepath: {basepath}")
     generate_new_public_dir()
-    generate_pages_recursive(CONTENT_PATH, TEMPLET_FILE, PUBLIC_PATH)
+    generate_pages_recursive(CONTENT_PATH, TEMPLET_FILE, PUBLIC_PATH, basepath)
+    
 
 if __name__ == "__main__":
     main()
